6c446180b36e26fd404fa7f0f9e8fb2ffb926844
michellisx <100141052+michellisx@users.noreply.github.com>
2022-10-06 11:18:30 -0300

    sidebar att

--- src/view/components/sidebarMenu.jsx
+++ src/view/components/sidebarMenu.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import {LogoBranca, LogoCliente} from '../../logos';
+import {Logo,LogoCliente} from '../../logos';
+import 'boxicons';
+
 
 
 const Sidebar = styled.nav`
@@ -12,58 +14,73 @@ display: flex;
 flex-direction: column;
 border-radius: 20px;
 box-shadow: 1px 1px 10px 5px  #e9e9e9;
+
+/* Aqui temos o heder do menu sidebar */
  .side-header{
     background-image: radial-gradient( #a2368c, #2a1225);
-    height: 20vh;
+    height: 22vh;
     border-radius: 0px 20px 0px 0px;
     justify-content: center;
     display: flex;
  }
-  .logoRFF{
-        width: 10vw;
+   .logoRFF{
+        width: 9vw;
         height: 5vh;
         margin-top: 26px;
   }
-  .logoRFF img{
+   .logoRFF img{
         width: 100%;
         
   }
+  
+    .spnomelogo{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 11vh;
+    }
 
-  .foto{
-    width: 80px;
-    height: 80px;
-    background-image:url(${LogoCliente}); 
-    background-position:center;
-    background-size:100px;
-    background-repeat:no-repeat;
-    border-radius: 50px;
-    position: absolute;
-    top:90px;
-    
-    
+    .foto{
+     width: 5.9vw;
+     height: 12.2vh;
+     background-image:url(${LogoCliente});  
+     background-position:center;
+     background-size:100px;
+     background-repeat:no-repeat;
+     border-radius: 50px;
+     position: absolute;
+     top:90px;
   }
-  .borderfoto{
-      width: 95px;
-      height: 95px;
-      background-color: #2a12251c;
+      
+      .borderfoto{
+      width: 6.9vw;
+      height: 14.5vh;
+      background-color: #00000024;
       position: fixed;
       border-radius: 50px;
       top: 82px;
     }
     
-  .spnomelogo{
-display: flex;
-justify-content: center;
-align-items: center;
+     .nomecliente{
+     font-size:19px;
+     font-family:'Poppins', sans-serif;
+     font-weight:600;
+     margin-top: 40px;
   }
-  .nomecliente{
-    font-size:19px;
-    font-family:'Poppins', sans-serif;
-    font-weight:600;
-    margin-top: 10px;
-  }
+
+
+/* Aqui começa a estilização dos itens do menu */
+.itensmenu{
+  height: 69vh;
+  padding-top: 30px;
+}
+
+.itensmenu li{
+  list-style: none;
+}
  
 `;
+
 
 export default function SidebarMenu(props){
     return(
@@ -72,7 +89,7 @@ export default function SidebarMenu(props){
     <Sidebar>
         <div className="side-header">
             <div className="logoRFF">
-                <img src={LogoBranca} alt="" />
+                <img src={Logo} alt="" />
             </div>
         </div>
         <div className="spnomelogo">
@@ -99,6 +116,15 @@ export default function SidebarMenu(props){
                 <box-icon color ='#2a122575'name='basket'></box-icon>
                 <span className='NomeC'>Pedidos</span>
                 </a>
+              </li>
+              <li>
+                <div className="drop">
+                <a href="https://plataforma.fastfoodbr.com/">
+                <box-icon color='#2a122575'name='dish'></box-icon>
+                <span className='NomeC'>Cardápio</span>
+                </a>
+                <box-icon type='solid' name='chevron-down'></box-icon>
+                </div>
               </li>
             </ul>
     </Sidebar>
